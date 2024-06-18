0cde80f19512db7c160c08457a21cafcf97ff617
chernikov22 <chernikov.v@gmail.com>
2024-06-18 14:43:44 +0700

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,7 @@
+immutable_var = (33,45, False, 'Nvidia')
+print('Immutable tuple: ',immutable_var)
+mutable_list = [43, 78, 45, 81]
+# immutable_var[1] = 11 Изменить эдемент кортежа невозможно, это неизменяемая упорядоченная коллекция
+mutable_list[1] = 'Изменненный'
+print('Mutable list: ',mutable_list)
+
