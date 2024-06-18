6ae627e8da3e6dfa8ecef4387f67b7655861bf51
chernikov22 <chernikov.v@gmail.com>
2024-06-18 14:47:54 +0700

    Update Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -1,4 +1,4 @@
-immutable_var = (33,45, False, 'Nvidia')
+immutable_var = (33, 45, False, 'Nvidia')
 print('Immutable tuple: ',immutable_var)
 mutable_list = [43, 78, 45, 81]
 # immutable_var[1] = 11 Изменить эдемент кортежа невозможно, это неизменяемая упорядоченная коллекция
