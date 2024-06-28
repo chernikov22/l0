094914dadf0df5763ddf2351f173a666adc79ff2
Vladimir <chernikov.v@gmail.com>
2024-06-28 22:33:37 +0700

    Add files via upload

---
+++ Homework2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while (index < len(my_list)):
+    if (my_list[index]>0):
+        print (str(my_list[index])+"\n")
+        index+=1
+        continue
+
+    if (my_list[index]==0):
+        index+=1
+        continue
+
+    if (my_list[index]<0):
+        break
